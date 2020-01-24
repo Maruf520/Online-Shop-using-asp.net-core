5ce67686b283decd578dffff5c452b9cdb9300a4
Maruf520 <maruf02@student.sust.edu>
2020-01-24 12:18:13 +0600

    View in ProductTypeController

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EO.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
+using OnlineShop.Models;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -15,15 +17,38 @@ namespace OnlineShop.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private ApplicationDbContext _db;
-
+  
         public ProductController(ApplicationDbContext db)
         {
             _db = db;
 
         }
-        public IActionResult Index()
+        [HttpGet]
+        public ActionResult Index()
         {
-            return View(_db.Products.Include(c=>c.ProductTypes).Include(f=>f.SpecialTagId).ToList());
+            return View(_db.Products.Include(p=>p.ProductTypes).Include(c => c.ProductTypes).ToList());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create (Products products)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+
+
+        }
+
+
     }
 }
